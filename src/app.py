7db40fa1b2387c9f6ebb67fbfeeb444ff0472bd4
peiyao <peiyaochang@qnap.com>
2020-01-10 14:41:21 +0800

    change content to real project code

--- src/app.py
+++ src/app.py
@@ -3,28 +3,17 @@ from flask import Flask, render_template
 # make an object with the imported Flask module. This object will be our WSGI application called app
 app = Flask(__name__)
 
-
 @app.route("/")
-def hello():
+def homepage():
+    """View function for Home Page."""
     # render_template 會自動去當前目錄下的 templates folder 找尋指定的 template 來使用
     return render_template("home.html")
 
 
-@app.route("/educative")
-def learn():
-    return "Happy Learning at Educative!"
-
-
-@app.route("/name/<my_name>")
-def greatings(my_name):
-    """View function to greet the user by name."""
-    return "Welcome " + my_name + "!"
-
-
-@app.route('/square/<int:number>')
-def show_square(number):
-    """View that shows the square of the number passed by URL"""
-    return "Square of " + str(number) + " is: " + str(number * number)
+@app.route("/about")
+def about():
+    """View function for About Page."""
+    return render_template("about.html")
 
 
 if __name__ == "__main__":
