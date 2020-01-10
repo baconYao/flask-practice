9526c56da2950330fc60b374e77224c53e40f26a
peiyao <peiyaochang@qnap.com>
2020-01-10 14:34:06 +0800

    added some urls and use template to render html page

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # make an object with the imported Flask module. This object will be our WSGI application called app
 app = Flask(__name__)
@@ -6,7 +6,24 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template("home.html")
+
+
+@app.route("/educative")
+def learn():
+    return "Happy Learning at Educative!"
+
+
+@app.route("/name/<my_name>")
+def greatings(my_name):
+    """View function to greet the user by name."""
+    return "Welcome " + my_name + "!"
+
+
+@app.route('/square/<int:number>')
+def show_square(number):
+    """View that shows the square of the number passed by URL"""
+    return "Square of " + str(number) + " is: " + str(number * number)
 
 
 if __name__ == "__main__":
